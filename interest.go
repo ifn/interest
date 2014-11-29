f3b2e44b7f8a3bebf0e1ba81781bafce41c63f3c
ifn <shabaev.shat@gmail.com>
2014-11-29 13:09:13 +0400

    added CompoundInterest

--- interest.go
+++ interest.go
@@ -10,48 +10,75 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func simpleInterestHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func interestHandler(si SetInterest) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 
-	decoder := json.NewDecoder(r.Body)
-	encoder := json.NewEncoder(w)
+		decoder := json.NewDecoder(r.Body)
+		encoder := json.NewEncoder(w)
 
-	var si SimpleInterest
+		err := decoder.Decode(&si)
+		if err != nil {
+			encoder.Encode(Error{"Error decoding json"})
+			return
+		}
 
-	err := decoder.Decode(&si)
-	if err != nil {
-		encoder.Encode(Error{"Error decoding json"})
-		return
-	}
+		si.SetInterest()
 
-	si.SetSimpleInterest()
-
-	err = encoder.Encode(si)
-	if err != nil {
-		//TODO: using the same encoder instance. is it ok?
-		encoder.Encode(Error{"Error encoding json"})
+		err = encoder.Encode(si)
+		if err != nil {
+			//TODO: using the same encoder instance. is it ok?
+			encoder.Encode(Error{"Error encoding json"})
+		}
 	}
 }
+
+var simpleInterestHandler http.HandlerFunc = interestHandler(&SimpleInterest{})
+var compoundInterestHandler http.HandlerFunc = interestHandler(&CompoundInterest{})
 
 type Error struct {
 	Err string `json:"error"`
 }
 
-type SimpleInterest struct {
+type SetInterest interface {
+	SetInterest()
+}
+
+type SimpleInterestRequest struct {
 	Principal float64 `json:"principal"`
 	Interest  float64 `json:"interest"`
 	Period    float64 `json:"period"`
+}
+
+type SimpleInterest struct {
+	*SimpleInterestRequest
 
 	SimpleInterest float64 `json:"simpleInterest"`
 }
 
-func (self *SimpleInterest) SetSimpleInterest() {
+func (self *SimpleInterest) SetInterest() {
 	self.SimpleInterest = self.Principal * (self.Interest * 1e-2) * self.Period
+}
+
+type CompoundInterestRequest struct {
+	*SimpleInterestRequest
+	Frequency int `json:"frequency"`
+}
+
+type CompoundInterest struct {
+	*CompoundInterestRequest
+
+	CompoundInterest float64 `json:"compoundInterest"`
+}
+
+func (self *CompoundInterest) SetInterest() {
+	self.CompoundInterest = 0
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/simpleInterest", simpleInterestHandler).Methods("POST")
+	r.HandleFunc("/compoundInterest", compoundInterestHandler).Methods("POST")
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
